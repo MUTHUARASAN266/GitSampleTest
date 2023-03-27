050fe30b4462f8fd195071c1e7c781a37e5b026a
CIPL1251 <muthuarasan@colanonline.com>
2023-03-27 18:06:03 +0530

    changes on main branch

--- app/src/main/java/com/muthu/gitsampletest/MainActivity.kt
+++ app/src/main/java/com/muthu/gitsampletest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.muthu.gitsampletest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//alter
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
